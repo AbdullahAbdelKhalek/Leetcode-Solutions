97018ab9aa86c4629222f695a5589f4a6fcba1ae
Abdullah Abdel-Khalek <abdullahhussamak@gmail.com>
2024-12-12 19:01:27 -0600

    Create add_two_integers.py

---
+++ Easy/add_two_integers.py
@@ -0,0 +1,15 @@
+# Problem: 2235. Add Two Integers
+# Link: https://leetcode.com/problems/add-two-integers/
+# Description: Given two integers num1 and num2, return the sum of the two integers.
+# Example 1:
+#   Input: num1 = 12, num2 = 5
+#   Output: 17
+# Example 2:
+#   Input: num1 = -10, num2 = 4
+#   Output: -6
+# Constraints:
+#   -100 <= num1, num2 <= 100
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
