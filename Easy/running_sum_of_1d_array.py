732eb5f1dea2461edf7a2e70649fb52b4bd682b0
Abdullah Abdel-Khalek <abdullahhussamak@gmail.com>
2024-12-12 20:03:22 -0600

    Update running_sum_of_1d_array.py

--- Easy/running_sum_of_1d_array.py
+++ Easy/running_sum_of_1d_array.py
@@ -1,4 +1,4 @@
-# Problem: Running Sum of 1d Array
+# Problem: 1480. Running Sum of 1d Array
 # Link: https://leetcode.com/problems/running-sum-of-1d-array/
 # Difficulty: Easy
 # Description: Given an array `nums`, return the running sum of the array.
